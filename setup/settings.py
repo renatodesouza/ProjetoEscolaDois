927d5bf2e6a78dc3c2d5290cba46c38367e48c79
renatodesouza <rlucas.rs2@gmail.com>
2023-07-08 15:32:35 -0300

    atualizando

--- setup/settings.py
+++ setup/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-u)r^#@e9=y^vk)49r2evj)ezv!s^o%ey9p_!)98#es#nwgpz3o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
